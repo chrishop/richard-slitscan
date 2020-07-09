496ddf1bcf385ddfcd57833a341aac87036d75a0
chrishop <christopher.hopkins@bbc.co.uk>
2020-07-09 23:50:28 +0100

    removed install

--- install.sh
+++
@@ -1,7 +0,0 @@
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-brew install python3
-brew install git
-git install https://github.com/chrishop/richard-slitscan.git
-cd richard-slitscan
-chmod +x slitscan-gui.py
-./slitscan-gui.py
